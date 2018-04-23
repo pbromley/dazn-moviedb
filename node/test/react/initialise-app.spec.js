5862cb8892495f41b5bd7ff2a7319512c101df6f
Philip Bromley <philip.bromley@dlapiper.com>
2018-04-23 21:16:14 +0100

    initial project setup

---
+++ node/test/react/initialise-app.spec.js
@@ -0,0 +1,32 @@
+'use strict';
+import React from 'react';
+import mock from 'mock-require';
+import {expect} from 'chai';
+import {spy} from 'sinon';
+
+describe('Initial app', () => {
+
+    const originalGetElementById = document.getElementById;
+
+    before(() => {
+        delete require.cache[require.resolve('react-dom')];
+    });
+
+    after(() => {
+        mock.stopAll();
+        document.getElementById = originalGetElementById;
+    });
+
+    it('should mount app onto correct id', () => {
+        document.getElementById = spy();
+        mock('react-dom', {
+            render: component => {
+                expect(component).to.deep.equal(<div></div>);
+                expect(document.getElementById.called).to.equal(true);
+                expect(document.getElementById.getCall(0).args[0]).to.equal('app');
+            }
+        });
+
+        require('../../src/react/initialise-app');
+    });
+});
